876d269ba16773f96aa186d68107481fcb4c8029
Franz Puerto <puertofranz@gmail.com>
2025-10-17 18:21:58 +0200

    [init]:spring init

---
+++ src/main/java/com/example/raftimplementation/RaftimplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.raftimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftimplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaftimplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raftimplementation/RaftimplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raftimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftimplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
