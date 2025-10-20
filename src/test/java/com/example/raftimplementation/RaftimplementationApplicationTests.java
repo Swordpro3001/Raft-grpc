bcd8750c96c99f583df0617263f69cc18dd45cc1
Franz Puerto <puertofranz@gmail.com>
2025-10-20 14:54:34 +0200

    [Feat]: Fixed Deprecated dependency Mockito

--- src/test/java/com/example/raftimplementation/RaftimplementationApplicationTests.java
+++ src/test/java/com/example/raftimplementation/RaftimplementationApplicationTests.java
@@ -3,18 +3,16 @@ package com.example.raftimplementation;
 import com.example.raftimplementation.service.RaftNode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class RaftimplementationApplicationTests {
 
-	@MockBean
+	@MockitoBean
 	private RaftNode raftNode;
 
 	@Test
 	void contextLoads() {
-		// Test that Spring context loads successfully
-		// RaftNode is mocked to avoid connection attempts to peers
 	}
 
 }
